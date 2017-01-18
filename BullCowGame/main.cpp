a7c22abcf93c691cc5731355f6aba4895ae41a17
chasemh <chase_hennion@outlook.com>
2017-01-17 21:41:54 -0600

    Adding work for lectures 21 through 30.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <string>
+#include "FBullCowGame.h"
 
 // # Designates a preprocessor directive
 // Use <> for including standard libraries
 // Use "" for your including your own header files
 
-using namespace std;
-
 // Method Signatures/Prototypes
 void PrintIntro();
 void PlayGame();
-string GetGuess();
+std::string GetGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame; // Instantiate a new game
 
 // Entry point for application
 int main() {
 
 	PrintIntro();
-	PlayGame();
+
+	do {
+		PlayGame();
+	} while ( AskToPlayAgain() );
+	
 	return 0;
 }
 
@@ -24,8 +30,8 @@ int main() {
 void PrintIntro() {
 	
 	constexpr int WORD_LENGTH = 9;
-	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 	return;
 }
 
@@ -33,18 +39,36 @@ void PrintIntro() {
 // Allow the Player to make a set number of guesses
 void PlayGame() {
 
-	constexpr int NUM_GUESSES = 5;
-	for (int i = 0; i < NUM_GUESSES; ++i) {
-		string Guess = GetGuess();
-		cout << "Your guess: " << Guess << endl << endl;
+	int MaxTries = BCGame.GetMaxTries();
+	std::cout << "Max Tries: " << MaxTries << std::endl;
+
+	//TODO Change from FOR to WHILE after adding validation
+	for (int i = 0; i < MaxTries; ++i) {
+		std::string Guess = GetGuess();
+		// TODO Add Guess Validation
+		// TODO Submit Valid Guess to Game
+		// TODO Print Number of Bulls and Cows
+		std::cout << "Your guess: " << Guess << std::endl << std::endl;
 	}
+
+	// TODO Add a Game summary
 }
 
 // Obtain a guess from the player
-string GetGuess() {
+std::string GetGuess() {
 	
-	cout << "Please guess an isogram: ";
-	string Guess = "";
-	getline(cin, Guess);
+	int CurrentTry = BCGame.GetCurrentTry();
+	std::cout << "Try " << CurrentTry << ": Please guess an isogram: ";
+	std::string Guess = "";
+	std::getline(std::cin, Guess);
 	return Guess;
+}
+
+bool AskToPlayAgain() {
+
+	std::cout << "Would you like to play again? (y/n): ";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+
+	return (Response[0] == 'y' || Response[0] == 'Y');
 }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 2017-01-17 Chase Hennion
+
+* Adding work that covers lectures 21 through 30 in Section 2.
+
 ## 2017-01-16 Chase Hennion
 
 * Adding initital work
